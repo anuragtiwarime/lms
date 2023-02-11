8658a503583370a57b5c2b9f582d4835eacb1eb6
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-02-11 11:11:24 +0530

    designed checkout card

--- client/src/Pages/Payment/Checkout.js
+++ client/src/Pages/Payment/Checkout.js
@@ -7,12 +7,13 @@ const Checkout = () => (
     {/* checkout page container */}
     <div className="min-h-[90vh] flex items-center justify-center text-white">
       {/* checkout card */}
-      <div className="w-80 h-[26rem] flex flex-col justify-center shadow-[0_0_10px_white] rounded-lg gap-6 relative">
-        <h1 className="bg-yellow-600 absolute top-0 w-full text-center py-4 text-2xl font-bold rounded-tl-lg rounded-tr-lg">
+      <div className="w-80 h-[26rem] flex flex-col justify-center shadow-[0_0_10px_white] rounded-lg relative">
+        <h1 className="bg-yellow-500 absolute top-0 w-full text-center py-4 text-2xl font-bold rounded-tl-lg rounded-tr-lg">
           Subscription Bundle
         </h1>
-        <div className="px-4 space-y-2">
-          <p>
+
+        <div className="px-4 space-y-5">
+          <p className="text-[17px]">
             This purchase will allow you to access all the available courses of
             our platform for{" "}
             <span className="text-yellow-500 font-bold">1 Year Duration</span>.{" "}
@@ -21,14 +22,17 @@ const Checkout = () => (
             in this subscription bundle
           </p>
 
-          <p className="flex items-center justify-center gap-1">
+          <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
             <BiRupee /> <span>499</span>only
           </p>
 
-          <p>100% refund at cancellation</p>
-          <p>* Terms & Condition Applied</p>
+          <div className="text-gray-200">
+            <p>100% refund at cancellation</p>
+            <p>* Terms & Condition Applied</p>
+          </div>
         </div>
-        <button className="bg-yellow-600 hover:bg-yellow-500 transition-all ease-in-out duration-300 absolute bottom-0 w-full text-center py-2 text-xl font-bold rounded-bl-lg rounded-br-lg">
+
+        <button className="bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full text-center py-2 text-xl font-bold rounded-bl-lg rounded-br-lg">
           Buy Now
         </button>
       </div>
