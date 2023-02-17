8252281d26562b6a01e9e974cfe2f59b63ca8705
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-02-17 16:34:26 +0530

    BE: Payment Model/Schema + verify sub route

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -4,6 +4,7 @@ import asyncHandler from '../middlewares/asyncHandler.middleware.js';
 import User from '../models/user.model.js';
 import AppErr from '../utils/appErr.js';
 import { razorpay } from '../server.js';
+import Payment from '../models/Payment.model.js';
 
 export const subscribe = asyncHandler(async (req, res, next) => {
   // Extracting ID from request obj
@@ -23,6 +24,8 @@ export const subscribe = asyncHandler(async (req, res, next) => {
     customer_notify: 1, // 1 means razorpay will handle notifying the customer, 0 means we will not notify the customer
     total_count: 12, // 12 means it will charge every month for a 1-year sub.
   });
+
+  console.log(subscription);
 
   // Adding the ID and the status to the user account
   user.subscription.id = subscription.id;
@@ -49,11 +52,23 @@ export const verifySubscription = asyncHandler(async (req, res, next) => {
 
   const generatedSignature = crypto
     .createHmac('sha256', process.env.RAZORPAY_SECRET)
-    .update(`${razorpay_payment_id} | ${razorpay_subscription_id}`)
+    .update(`${razorpay_payment_id}|${subscriptionId}`)
     .digest('hex');
 
   if (generatedSignature !== razorpay_signature) {
     return res.redirect(process.env.FRONTEND_URL + '/payment-failed');
     // return next(new AppErr('Payment not successful', 400));
   }
+
+  await Payment.create({
+    razorpay_payment_id,
+    razorpay_subscription_id,
+    razorpay_signature,
+  });
+
+  user.subscription.status = 'active';
+
+  await user.save();
+
+  res.redirect(process.env.FRONTEND_URL + '/payment-success');
 });
---
+++ server/models/Payment.model.js
@@ -0,0 +1,25 @@
+import { model, Schema } from 'mongoose';
+
+const paymentSchema = new Schema(
+  {
+    razorpay_payment_id: {
+      type: String,
+      required: true,
+    },
+    razorpay_subscription_id: {
+      type: String,
+      required: true,
+    },
+    razorpay_signature: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Payment = model('Payment', paymentSchema);
+
+export default Payment;
