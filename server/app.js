1ac7933b159a2c2ba71d4ea2eff9574a672d04eb
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-02-24 14:06:46 +0530

    BE: Fix the GitHub merge error

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 config();
-import express from "express";
-import { config } from "dotenv";
-import cors from "cors";
-import morgan from "morgan";
-import errorMiddleware from "./middlewares/error.middleware.js";
+import express from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
@@ -19,31 +19,26 @@ app.use(
     credentials: true,
   })
 );
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cookieParser());
 
 // Server Status Check Route
-app.get("/ping", (_req, res) => {
-  res.send("Pong");
+app.get('/ping', (_req, res) => {
+  res.send('Pong');
 });
 
 // Import all routes
-import userRoutes from "./routes/user.routes.js";
-import courseRoutes from "./routes/course.routes.js";
-import paymentRoutes from "./routes/payment.routes.js";
+import userRoutes from './routes/user.routes.js';
+import courseRoutes from './routes/course.routes.js';
+import paymentRoutes from './routes/payment.routes.js';
 
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/courses", courseRoutes);
-app.use("/api/v1/payments", paymentRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/courses', courseRoutes);
+app.use('/api/v1/payments', paymentRoutes);
 
 // Default catch all route - 404
-<<<<<<< HEAD
-app.all("*", (_req, res) => {
-  res.send("OOPS!!! 404 Page Not Found");
-=======
 app.all('*', (_req, res) => {
   res.status(404).send('OOPS!!! 404 Page Not Found');
->>>>>>> d633b4d5ee50ba336ee8cbb135ed4ae13d65ebbe
 });
 
 // Custom error handling middleware
