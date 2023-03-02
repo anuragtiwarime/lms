9c8a2ba611b8132e67c4d897903790951dd7e809
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-03-02 14:38:42 +0530

    added: create new course page

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "../Redux/authSlice";
+import courseSliceReducer from "../Redux/courseSlice";
 
 const store = configureStore({
   reducer: {
     auth: authSliceReducer,
+    course: courseSliceReducer,
   },
 });
 
