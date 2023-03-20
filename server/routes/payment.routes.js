260e07c6a9a9735f3056c0bf03c6785bf5ccf6b0
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-03-20 10:46:26 +0530

    BE: Add authorizeSubscribers mdlr for cancel sub route

--- server/routes/payment.routes.js
+++ server/routes/payment.routes.js
@@ -5,13 +5,18 @@ import {
   verifySubscription,
   cancelSubscription,
 } from '../controllers/payment.controller.js';
-import { isLoggedIn } from '../middlewares/auth.middleware.js';
+import {
+  authorizeSubscribers,
+  isLoggedIn,
+} from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.route('/subscribe').post(isLoggedIn, buySubscription);
 router.route('/verify').post(isLoggedIn, verifySubscription);
-router.route('/unsubscribe').post(isLoggedIn, cancelSubscription);
+router
+  .route('/unsubscribe')
+  .post(isLoggedIn, authorizeSubscribers, cancelSubscription);
 router.route('/razorpay-key').get(getRazorpayApiKey);
 
 export default router;
