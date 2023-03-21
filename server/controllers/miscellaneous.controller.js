71c9a6791cd461ffa877d96e7f5ab8e46a60f6e1
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-03-21 11:10:59 +0530

    BE: Added comments and protected routes

--- server/controllers/miscellaneous.controller.js
+++ server/controllers/miscellaneous.controller.js
@@ -9,8 +9,10 @@ import sendEmail from '../utils/sendEmail.js';
  * @ACCESS Public
  */
 export const contactUs = asyncHandler(async (req, res, next) => {
+  // Destructuring the required data from req.body
   const { name, email, message } = req.body;
 
+  // Checking if values are valid
   if (!name || !email || !message) {
     return next(new AppErr('Name, Email, Message are required'));
   }
@@ -19,6 +21,7 @@ export const contactUs = asyncHandler(async (req, res, next) => {
     const subject = 'Contact Us Form';
     const textMessage = `${name} - ${email} <br /> ${message}`;
 
+    // Await the send email
     await sendEmail(process.env.CONTACT_US_EMAIL, subject, textMessage);
   } catch (error) {
     console.log(error);
@@ -40,7 +43,7 @@ export const userStats = asyncHandler(async (req, res, next) => {
   const allUsersCount = await User.countDocuments();
 
   const subscribedUsersCount = await User.countDocuments({
-    'subscription.status': 'active',
+    'subscription.status': 'active', // subscription.status means we are going inside an object and we have to put this in quotes
   });
 
   res.status(200).json({
--- server/routes/miscellaneous.routes.js
+++ server/routes/miscellaneous.routes.js
@@ -3,11 +3,14 @@ import {
   contactUs,
   userStats,
 } from '../controllers/miscellaneous.controller.js';
+import { authorizeRoles, isLoggedIn } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 // {{URL}}/api/v1/
 router.route('/contact').post(contactUs);
-router.route('/admin/stats/users').get(userStats);
+router
+  .route('/admin/stats/users')
+  .get(isLoggedIn, authorizeRoles('ADMIN'), userStats);
 
 export default router;
