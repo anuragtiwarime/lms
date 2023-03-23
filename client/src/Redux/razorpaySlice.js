93070bd25896457b76c174464bb9a65a6880913b
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-03-23 16:41:07 +0530

    added razorpay payment from frontend

--- client/src/Redux/razorpaySlice.js
+++ client/src/Redux/razorpaySlice.js
@@ -4,15 +4,48 @@ import axiosInstance from "../Helper/axiosInstance";
 
 const initialState = {
   key: "",
+  subscription_id: "",
 };
 
 // function to get the api key
-export const getRazorPayId = createAsyncThunk("razorPayId/get", async () => {
+export const getRazorPayId = createAsyncThunk("/razorPayId/get", async () => {
   try {
     const res = await axiosInstance.get("/payments/razorpay-key");
-    return res;
+    return res.data;
   } catch (error) {}
 });
+
+// function to purchase the course bundle
+export const purchaseCourseBundle = createAsyncThunk(
+  "/purchaseCourse",
+  async () => {
+    try {
+      const res = await axiosInstance.post("/payments/subscribe");
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
+
+// function to verify the user payment
+export const verifyUserPayment = createAsyncThunk(
+  "/verifyPayment",
+  async (paymentDetail) => {
+    // console.log(...paymentDetail);
+    try {
+      const res = await axiosInstance.post("/payments/verify", {
+        razorpay_payment_id: paymentDetail.razorpay_payment_id,
+        razorpay_subscription_id: paymentDetail.razorpay_subscription_id,
+        razorpay_signature: paymentDetail.razorpay_signature,
+      });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 const razorpaySlice = createSlice({
   name: "razorpay",
@@ -24,7 +57,13 @@ const razorpaySlice = createSlice({
         toast.error("Failed to get razor pay id");
       })
       .addCase(getRazorPayId.fulfilled, (state, action) => {
-        state.key = action.payload.data.key;
+        state.key = action?.payload?.key;
+      })
+      .addCase(purchaseCourseBundle.fulfilled, (state, action) => {
+        state.subscription_id = action.payload.subscription_id;
+      })
+      .addCase(verifyUserPayment.fulfilled, (state, action) => {
+        console.log(action.payload);
       });
   },
 });
