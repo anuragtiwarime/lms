a12fa7a1764f1cc3ee3d174d9f92ecc87eb113c4
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-07-03 11:33:57 +0530

    fix: razorpay key fetch permission issue

--- server/routes/payment.routes.js
+++ server/routes/payment.routes.js
@@ -19,9 +19,7 @@ router.route('/verify').post(isLoggedIn, verifySubscription);
 router
   .route('/unsubscribe')
   .post(isLoggedIn, authorizeSubscribers, cancelSubscription);
-router
-  .route('/razorpay-key')
-  .get(isLoggedIn, authorizeRoles('ADMIN'), getRazorpayApiKey);
+router.route('/razorpay-key').get(isLoggedIn, getRazorpayApiKey);
 router.route('/').get(isLoggedIn, authorizeRoles('ADMIN'), allPayments);
 
 export default router;
